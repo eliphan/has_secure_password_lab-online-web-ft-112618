61c0dff97b5b79b0e710b64d534e6069d27ad324
Elise Phan <aliceconnic@gmail.com>
2019-02-20 02:57:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,8 @@
 ActiveRecord::Schema.define(version: 20190220021632) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

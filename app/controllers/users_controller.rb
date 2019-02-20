e4a15d09e5245ddd7defee37fb98cb8dc56a1c76
Elise Phan <aliceconnic@gmail.com>
2019-02-20 02:27:23 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  has_secure_password
+  
+  
+  
+  
+  
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end

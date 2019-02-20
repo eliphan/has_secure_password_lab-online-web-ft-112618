e2d35846ca5e2c6add56d29a1d50f2a07cbc442d
Elise Phan <aliceconnic@gmail.com>
2019-02-20 02:54:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   
   def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    user = User.find_by(username: params[:username])
+    authenticated = user.try(:authenticate, params[:password])
+    return head(:forbidden) unless authenticated
+    @user = user
     session[:user_id] = @user.id
   end
   
